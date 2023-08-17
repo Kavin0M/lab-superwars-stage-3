6f9029493d915baaceb79b04d8cb90f469ae7183
Kavin <kavinsai88@gmail.com>
2023-08-17 16:21:48 +0530

    Did some change

--- src/app.js
+++ src/app.js
@@ -21,20 +21,40 @@ const PLAYERS = [
     "Slingo"
 ];
 
-// initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
-
-    return detailedPlayers;
-}
-
 // getting random strength
 const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
+
+// initialize players with image and strength
+const initPlayers = (players) => {
+    let detailedPlayers = [];
+
+    // Instead of forloop use Map method
+    // Code here
+    detailedPlayers = players.map((item,index)=>{
+        if (index % 2 == 0){
+            return {
+                name: item,
+                strength: getRandomStrength(),
+                image: "images/super-"+(index+1)+".png",
+                type: "hero"
+            }
+        }
+        else{
+            return {
+                name: item,
+                strength: getRandomStrength(),
+                image: "images/super-"+(index+1)+".png",
+                type: "villain"
+            }
+        }
+    })
+
+    return detailedPlayers;
+}
+
+console.log(initPlayers(PLAYERS))
 
 // Build player template
 const buildPlayers = (players, type) => {
@@ -43,8 +63,19 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    var x = players.filter((player)=>{
+        return player.type == type
+    })
+   
+    var y = x.map((player,i)=>{
+        console.log(i)
+        let frag = `<div class="player"><img src="${player.image}" alt=""><div class="name">${player.name}</div><div class="strength">${player.strength}</div></div>`
+        return frag
+    })
 
-    return fragment;
+    fragment = y.join(' ')
+
+    return fragment
 }
 
 // Display players in HTML
